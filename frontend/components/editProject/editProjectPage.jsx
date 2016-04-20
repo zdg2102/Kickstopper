0ba285f0837804aa1bc5af7ae73636c71bd1b1bd
zdg2102 <zacharydgarcia@gmail.com>
2016-04-20 18:01:54 -0400

    Add favicon

--- frontend/components/editProject/editProjectPage.jsx
+++ frontend/components/editProject/editProjectPage.jsx
@@ -1,40 +1,72 @@
 // main component for project/unlaunched project edit page
 
 var React = require('react');
+var CategoryStore = require('../../stores/categoryStore');
+var ApiUtil = require('../../utils/apiUtil');
 
 var EditProjectPage = React.createClass({
   getInitialState: function () {
     return {
+      categoryTree: [],
       mainImageUrl: "",
       mainImageFile: null,
       secondaryImageUrl: "",
       secondaryImageFile: null,
       title: "",
+      category: "",
+      subcategory: "",
       blurb: "",
       duration: "",
       goal: "",
       description: "",
       rewardOneMin: "",
+      rewardOneTitle: "",
       rewardOneDesc: "",
       rewardTwoMin: "",
+      rewardTwoTitle: "",
       rewardTwoDesc: "",
       rewardThreeMin: "",
+      rewardThreeTitle: "",
       rewardThreeDesc: "",
       mainImageError: false,
       secondaryImageError: false,
       titleError: false,
+      categoryError: false,
+      subcategoryError: false,
       blurbError: false,
       durationError: false,
       goalError: false,
       descriptionError: false,
       rewardOneMinError: false,
+      rewardOneTitleError: false,
       rewardOneDescError: false,
       rewardTwoMinError: false,
+      rewardTwoTitleError: false,
       rewardTwoDescError: false,
       rewardThreeMinError: false,
+      rewardThreeTitleError: false,
       rewardThreeDescError: false,
       errorMessages: []
     };
+  },
+
+  componentDidMount: function () {
+    this.categoryStoreToken = CategoryStore
+      .addListener(this.updateCategories);
+    ApiUtil.getCategoryTree();
+    if (this.props.location.query.category) {
+      this.setState({ category: this.props.location.query.category });
+    }
+  },
+
+  componentWillUnmount: function () {
+    this.categoryStoreToken.remove();
+  },
+
+  updateCategories: function (arg) {
+    var category = arg || this.state.category
+    var newTree = CategoryStore.prunedTree(category);
+    this.setState({ categoryTree: newTree });
   },
 
   handleImageUpload: function (imageName, e) {
@@ -59,14 +91,130 @@ var EditProjectPage = React.createClass({
   },
 
   handleSubmit: function (e) {
+    e.preventDefault();
 
+    var newState = {};
+    newState.errorMessages = [];
+    if (this.state.mainImageUrl.length === 0) {
+      newState.mainImageError = true;
+      newState.errorMessages.push("Must have main image");
+    }
+    if (this.state.secondaryImageUrl.length === 0) {
+      newState.secondaryImageError = true;
+      newState.errorMessages.push("Must have secondary image");
+    }
+    if (this.state.title.length === 0) {
+      newState.titleError = true;
+      newState.errorMessages.push("Must have title");
+    }
+    if (this.state.category.length === 0) {
+      newState.categoryError = true;
+      newState.errorMessages.push("Must have category");
+    }
+    if (this.state.subcategory.length === 0) {
+      newState.subcategoryError = true;
+      newState.errorMessages.push("Must have subcategory");
+    }
+    if (this.state.duration.length === 0) {
+      newState.durationError = true;
+      newState.errorMessages.push("Must have a funding duration");
+    } else if (!parseInt(this.state.duration, 10) > 0) {
+      newState.durationError = true;
+      newState.errorMessages.push("Funding duration must be greater " +
+      "than zero");
+    }
+    if (this.state.goal.length === 0) {
+      newState.goalError = true;
+      newState.errorMessages.push("Must have a funding goal");
+    } else if (!parseInt(this.state.goal, 10) > 0) {
+      newState.goalError = true;
+      newState.errorMessages.push("Funding goal must be greater " +
+      "than zero");
+    }
+    if (this.state.rewardOneMin.length === 0) {
+      newState.rewardOneMinError = true;
+      newState.errorMessages.push("Must have a minimum pledge for " +
+      "Reward #1");
+    } else if (!parseInt(this.state.rewardOneMin, 10) > 0) {
+      newState.rewardOneMinError = true;
+      newState.errorMessages.push("Reward #1 minimum pledge must " +
+      "be greater than zero");
+    }
+
+    if (newState.errorMessages.length > 0) {
+      this.setState(newState);
+    } else {
+      this.saveProject();
+    }
   },
 
   saveProject: function () {
 
+    console.log("yay");
+  },
+
+  handleTextInput: function (fieldName, e) {
+    var newState = {};
+    newState[fieldName] = e.currentTarget.value;
+    newState[fieldName + "Error"] = false;
+    newState.errorMessages = [];
+    this.setState(newState);
+    // additionally, if the category selection was updated, also
+    // update the category tree
+    if (fieldName === "category") { this
+      .updateCategories(e.currentTarget.value); }
+  },
+
+  handleIntInput: function (fieldName, e) {
+    var intReg = /^\d*$/;
+    if (e.currentTarget.value.match(intReg)) {
+      var newState = {};
+      newState[fieldName] = e.currentTarget.value;
+      newState[fieldName + "Error"] = false;
+      newState.errorMessages = [];
+      this.setState(newState);
+    }
   },
 
   render: function () {
+    var mainImageError, secondaryImageError, titleError,
+      categoryError, subcategoryError, blurbError, durationError,
+      goalError, descriptionError, rewardOneMinError,
+      rewardOneDescError, rewardTwoMinError, rewardTwoDescError,
+      rewardThreeMinError, rewardThreeDescError, rewardOneTitleError,
+      rewardTwoTitleError, rewardThreeTitleError;
+
+    mainImageError = this.state.mainImageError ? " error" : "";
+    secondaryImageError = this.state.secondaryImageError ? " error" : "";
+    titleError = this.state.titleError ? " error" : "";
+    categoryError = this.state.categoryError ? " error" : "";
+    subcategoryError = this.state.subcategoryError ? " error" : "";
+    blurbError = this.state.blurbError ? " error" : "";
+    durationError = this.state.durationError ? " error" : "";
+    goalError = this.state.goalError ? " error" : "";
+    descriptionError = this.state.descriptionError ? " error" : "";
+    rewardOneMinError = this.state.rewardOneMinError ? " error" : "";
+    rewardOneTitleError = this.state.rewardOneTitleError ? " error" : "";
+    rewardOneDescError = this.state.rewardOneDescError ? " error" : "";
+    rewardTwoMinError = this.state.rewardTwoMinError ? " error" : "";
+    rewardTwoTitleError = this.state.rewardTwoTitleError ? " error" : "";
+    rewardTwoDescError = this.state.rewardTwoDescError ? " error" : "";
+    rewardThreeMinError = this.state.rewardThreeMinError ? " error" : "";
+    rewardThreeTitleError = this.state.rewardThreeTitleError ? " error" : "";
+    rewardThreeDescError = this.state.rewardThreeDescError ? " error" : "";
+
+    var errorList;
+    if (this.state.errorMessages.length > 0) {
+      errorList = this.state.errorMessages.map( function (error, idx) {
+        return <span key={idx} className="edit-error">{error}</span>;
+      });
+      errorList = (
+        <div className="edit-error-list">
+          {errorList}
+        </div>
+      );
+    }
+
     var mainImage, secondaryImage;
     if (this.state.mainImageUrl.length > 0) {
       mainImage = <img className="project-edit-preview"
@@ -75,6 +223,28 @@ var EditProjectPage = React.createClass({
     if (this.state.secondaryImageUrl.length > 0) {
       secondaryImage = <img className="project-edit-preview"
         src={this.state.secondaryImageUrl} />;
+    }
+
+    var categoryOptions = [];
+    var subcategoryOptions = [];;
+    for (var i = 0; i < this.state.categoryTree.length; i++) {
+      categoryOptions.push(
+        <option key={this.state.categoryTree[i].nameParam}
+          value={this.state.categoryTree[i].nameParam}>
+          {this.state.categoryTree[i].name}
+        </option>
+      );
+      if (this.state.categoryTree[i].subcategories) {
+        var subcats = this.state.categoryTree[i].subcategories
+        for (var j = 0; j < subcats.length; j++) {
+          subcategoryOptions.push(
+            <option key={subcats[j].name_param}
+              value={subcats[j].name_param}>
+              {subcats[j].name}
+            </option>
+          );
+        }
+      }
     }
 
     return (
@@ -91,12 +261,16 @@ var EditProjectPage = React.createClass({
         <h3 className="project-edit-subheader">
           {"Add an image, a goal, and other important details."}
         </h3>
+
+        {errorList}
+
         <form className="project-edit-form">
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Main project image"}
             </label>
-            <input type="file" className="project-edit-image-upload"
+            <input type="file" className={"project-edit-image-upload"
+              + mainImageError}
               onChange={this.handleImageUpload.bind(this, "main")}>
             </input>
             {mainImage}
@@ -105,7 +279,8 @@ var EditProjectPage = React.createClass({
             <label className="project-edit-item-label">
               {"Secondary project image"}
             </label>
-            <input type="file" className="project-edit-image-upload"
+            <input type="file" className={"project-edit-image-upload"
+              + secondaryImageError}
               onChange={this.handleImageUpload.bind(this, "secondary")}>
             </input>
             {secondaryImage}
@@ -114,32 +289,69 @@ var EditProjectPage = React.createClass({
             <label className="project-edit-item-label">
               {"Project title"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              titleError}
+              value={this.state.title}
+              onChange={this.handleTextInput.bind(this, "title")}/>
+          </div>
+          <div className="project-edit-item-box group">
+            <label className="project-edit-item-label">
+              {"Project category"}
+            </label>
+            <select value={this.state.category}
+              className={"project-edit-item-select" + categoryError}
+              onChange={this.handleTextInput.bind(this, "category")}>
+              <option value="">{""}</option>
+              {categoryOptions}
+            </select>
+          </div>
+          <div className="project-edit-item-box group">
+            <label className="project-edit-item-label">
+              {"Project subcategory"}
+            </label>
+            <select value={this.state.subcategory}
+              className={"project-edit-item-select" + subcategoryError}
+              onChange={this.handleTextInput.bind(this, "subcategory")}>
+              <option value="">{""}</option>
+              {subcategoryOptions}
+            </select>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Short blurb"}
             </label>
-            <textarea className="project-edit-item-textarea">
+            <textarea className={"project-edit-item-textarea" +
+              blurbError}
+              value={this.state.blurb}
+              onChange={this.handleTextInput.bind(this, "blurb")}>
             </textarea>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Funding duration (in days)"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              durationError}
+              value={this.state.duration}
+              onChange={this.handleIntInput.bind(this, "duration")}/>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Funding goal"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              goalError}
+              value={this.state.goal}
+              onChange={this.handleIntInput.bind(this, "goal")}/>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Project description"}
             </label>
-            <textarea className="project-edit-item-textarea">
+            <textarea className={"project-edit-item-textarea" +
+              descriptionError}
+              value={this.state.description}
+              onChange={this.handleTextInput.bind(this, "description")}>
             </textarea>
           </div>
           <label className="project-edit-reward-label">
@@ -149,13 +361,28 @@ var EditProjectPage = React.createClass({
             <label className="project-edit-item-label">
               {"Minimum pledge"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              rewardOneMinError}
+              value={this.state.rewardOneMin}
+              onChange={this.handleIntInput.bind(this, "rewardOneMin")}/>
+          </div>
+          <div className="project-edit-item-box group">
+            <label className="project-edit-item-label">
+              {"Reward title"}
+            </label>
+            <input type="text" className={"project-edit-item-input" +
+              rewardOneTitleError}
+              value={this.state.rewardOneTitle}
+              onChange={this.handleTextInput.bind(this, "rewardOneTitle")}/>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Description"}
             </label>
-            <textarea className="project-edit-item-textarea">
+            <textarea className={"project-edit-item-textarea" +
+              rewardOneDescError}
+              value={this.state.rewardOneDesc}
+              onChange={this.handleTextInput.bind(this, "rewardOneDesc")}>
             </textarea>
           </div>
           <label className="project-edit-reward-label">
@@ -165,13 +392,28 @@ var EditProjectPage = React.createClass({
             <label className="project-edit-item-label">
               {"Minimum pledge"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              rewardTwoMinError}
+              value={this.state.rewardTwoMin}
+              onChange={this.handleIntInput.bind(this, "rewardTwoMin")}/>
+          </div>
+          <div className="project-edit-item-box group">
+            <label className="project-edit-item-label">
+              {"Reward title"}
+            </label>
+            <input type="text" className={"project-edit-item-input" +
+              rewardTwoTitleError}
+              value={this.state.rewardTwoTitle}
+              onChange={this.handleTextInput.bind(this, "rewardTwoTitle")}/>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Description"}
             </label>
-            <textarea className="project-edit-item-textarea">
+            <textarea className={"project-edit-item-textarea" +
+              rewardTwoDescError}
+              value={this.state.rewardTwoDesc}
+              onChange={this.handleTextInput.bind(this, "rewardTwoDesc")}>
             </textarea>
           </div>
           <label className="project-edit-reward-label">
@@ -181,13 +423,29 @@ var EditProjectPage = React.createClass({
             <label className="project-edit-item-label">
               {"Minimum pledge"}
             </label>
-            <input type="text" className="project-edit-item-input" />
+            <input type="text" className={"project-edit-item-input" +
+              rewardThreeMinError}
+              value={this.state.rewardThreeMin}
+              onChange={this.handleIntInput.bind(this, "rewardThreeMin")}/>
+          </div>
+          <div className="project-edit-item-box group">
+            <label className="project-edit-item-label">
+              {"Reward title"}
+            </label>
+            <input type="text" className={"project-edit-item-input" +
+              rewardThreeTitleError}
+              value={this.state.rewardThreeTitle}
+              onChange={this.handleTextInput
+                .bind(this, "rewardThreeTitle")}/>
           </div>
           <div className="project-edit-item-box group">
             <label className="project-edit-item-label">
               {"Description"}
             </label>
-            <textarea className="project-edit-item-textarea">
+            <textarea className={"project-edit-item-textarea" +
+              rewardThreeDescError}
+              value={this.state.rewardThreeDesc}
+              onChange={this.handleTextInput.bind(this, "rewardThreeDesc")}>
             </textarea>
           </div>
         </form>
